195190bc2afd94028e570611aca80f1b24bdb59c
Aaditya2207 <aadityaakshat304@gmail.com>
2025-12-10 15:18:56 +0530

    Footer Update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className="footer border-t border-[#33353F] border-l-transparent border-r-transparent text-white z-10">
       <div className="container p-12 flex justify-between items-center">
         {/* Fixed text */}
-        <span className="text-white font-semibold text-lg">&lt;/AK&gt;</span>
-        <p className="text-slate-600">Designed and Developed by Aaditya
+        <span className="text-white font-semibold text-lg">Designed and Developed by Aaditya</span>
+        <p className="text-slate-600">Copyright © 2025 Aaditya
 </p>
       </div>
     </footer>
